d2fcf3014bed4e82f0330280da0cfca870d5a7e2
Roshni <66792774+CeraMapleheart@users.noreply.github.com>
2023-03-16 15:50:52 +0530

    Set of jar files

---
+++ Records/21CYS/CB.EN.U4CYS21061/Java Programming/src/com/amrita/main/Main.java
@@ -0,0 +1,8 @@
+package com.amrita.main;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
---
+++ Records/21CYS/CB.EN.U4CYS21061/addition/src/com/amrita/addition/addition.java
@@ -0,0 +1,10 @@
+package com.amrita.addition;
+import java.util.Scanner:
+public class addition {
+    public static void main(int x, int y) {
+        x=50;
+        y=60;
+        int sum= x+y;
+        System.out.println(sum);
+    }
+}
