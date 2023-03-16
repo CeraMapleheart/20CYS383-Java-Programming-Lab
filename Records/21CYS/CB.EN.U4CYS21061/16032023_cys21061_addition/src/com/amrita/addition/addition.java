237de57a4371adc4830ffd914d9212cf2e821d2b
Roshni <66792774+CeraMapleheart@users.noreply.github.com>
2023-03-16 16:13:45 +0530

    jar files from lab

---
+++ Records/21CYS/CB.EN.U4CYS21061/16032023_cys21061_addition/src/com/amrita/addition/addition.java
@@ -0,0 +1,12 @@
+package com.amrita.addition;
+
+import java.util.Scanner;
+public class addition {
+    public static void main(String[] args) {
+        int x=50;
+        int y=60;
+        int sum= x+y;
+        System.out.println(sum);
+    }
+}
+
---
+++ Records/21CYS/CB.EN.U4CYS21061/16032023_cys21061_multiplication/src/multiplication.java
@@ -0,0 +1,12 @@
+package com.amrita.multiplication;
+
+import java.util.Scanner;
+public class multiplication {
+    public static void main(String[] args) {
+        int x=50;
+        int y=60;
+        int product= x*y;
+        System.out.println(product);
+    }
+}
+
